784edef0edc9e17f48691784da145200ae287333
Fred Castro <fred.cneto@hotmail.com>
2022-02-01 20:53:24 -0300

    included a generic timer decorator

--- utils/timer_funcs.py
+++ utils/timer_funcs.py
@@ -29,3 +29,35 @@ def improve_timer_decorator(improve_func):
 
     # returns the wrapper function
     return wrapper
+
+
+def timeit(func):
+    """
+    a decorator to time a function (mostly used for debugging)
+
+    :param func: the function to be timed
+    :type func: function
+    :return: the decorated func
+    :rtype: func
+    """
+    @wraps(func)
+    def new_func(*args, **kwargs):
+
+        # start time
+        start_time = time.time()
+
+        # execute the function
+        result = func(*args, **kwargs)
+
+        # compute elapsed time
+        elapsed_time = time.time() - start_time
+
+        # log the ending result
+        logger.info('function [{}] finished in {} ms'.format(
+            func.__name__, int(elapsed_time * 1_000)))
+
+        # returns the result of the function
+        return result
+
+    # returns the wrapper function
+    return new_func
