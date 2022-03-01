7751daf018e567e5d844e87301d8fc9da6cb574a
Fred Castro <fred.cneto@hotmail.com>
2022-03-01 20:01:06 -0300

    changed cost func naming

--- utils/cost_funcs.py
+++ utils/cost_funcs.py
@@ -1,6 +1,6 @@
-def cost_func_2d(n1, n2):
+def euc_2d(n1, n2):
     """
-    The cost function for 2D cartesian nodes, which is the euclidean distance between two nodes
+    The euclidean distance for 2D cartesian nodes
 
     :param n1: first node
     :type n1: Node
@@ -12,9 +12,9 @@ def cost_func_2d(n1, n2):
     return ((n1.x - n2.x)**2 + (n1.y - n2.y)**2)**0.5
 
 
-def cost_func_3d(n1, n2):
+def euc_3d(n1, n2):
     """
-    The cost function for 3D cartesian nodes, which is the euclidean distance between two nodes
+    The euclidean distance for 3D cartesian nodes
 
     :param n1: first node
     :type n1: Node3D
@@ -27,5 +27,5 @@ def cost_func_3d(n1, n2):
 
 
 # a dictionary mapping tsp lib edge weight type with respective cost function
-cost_funcs = {"EUC_2D": cost_func_2d,
-              "EUC_3D": cost_func_3d}
+cost_funcs = {"EUC_2D": euc_2d,
+              "EUC_3D": euc_3d}
