d4f15d5fe88223d98dea9af61b5f4202934ddd1a
Fred Castro <fred.cneto@hotmail.com>
2022-04-27 19:03:18 -0300

    removed unused solver scripts

--- src/lk_heuristic/solvers/tsp_interactive_solver.py
+++
@@ -1,8 +0,0 @@
-import logging
-from lk_heuristic.utils.solver_funcs import get_interactive_inputs, solve_tsp
-
-# start interactive shell to collect tsp file and solution method
-tsp_file, solution_method = get_interactive_inputs()
-
-# execute the solver using inputs
-solve_tsp(tsp_file, solution_method, 1, logging.DEBUG)
